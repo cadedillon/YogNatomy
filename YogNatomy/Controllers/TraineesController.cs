3648020234ee8ba0e3e9feaa3854ffb0267cc9c0
Cade Dillon <42156426+cadedillon@users.noreply.github.com>
2018-10-28 21:08:29 -0700

    Add project files.

---
+++ YogNatomy/Controllers/TraineesController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using YogNatomy.Models;
+
+namespace YogNatomy.Controllers
+{
+    public class TraineesController : Controller
+    {
+        private YogNatomyEntities db = new YogNatomyEntities();
+
+        // GET: Trainees
+        public ActionResult Index()
+        {
+            var trainees = db.Trainees.Include(t => t.Trainer);
+            return View(trainees.ToList());
+        }
+
+        // GET: Trainees/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Trainee trainee = db.Trainees.Find(id);
+            if (trainee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(trainee);
+        }
+
+        // GET: Trainees/Create
+        public ActionResult Create()
+        {
+            ViewBag.trainerid = new SelectList(db.Trainers, "trainerid", "trainername");
+            return View();
+        }
+
+        // POST: Trainees/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "traineeid,traineename,trainerid,traineeheight,traineeweight,traineeage,fitnesslevel")] Trainee trainee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Trainees.Add(trainee);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.trainerid = new SelectList(db.Trainers, "trainerid", "trainername", trainee.trainerid);
+            return View(trainee);
+        }
+
+        // GET: Trainees/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Trainee trainee = db.Trainees.Find(id);
+            if (trainee == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.trainerid = new SelectList(db.Trainers, "trainerid", "trainername", trainee.trainerid);
+            return View(trainee);
+        }
+
+        // POST: Trainees/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "traineeid,traineename,trainerid,traineeheight,traineeweight,traineeage,fitnesslevel")] Trainee trainee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(trainee).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.trainerid = new SelectList(db.Trainers, "trainerid", "trainername", trainee.trainerid);
+            return View(trainee);
+        }
+
+        // GET: Trainees/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Trainee trainee = db.Trainees.Find(id);
+            if (trainee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(trainee);
+        }
+
+        // POST: Trainees/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Trainee trainee = db.Trainees.Find(id);
+            db.Trainees.Remove(trainee);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
